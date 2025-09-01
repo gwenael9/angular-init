a355c741d94ff4a6ff4cde444fd647cc1bc47197
gwenael9 <gwenaelgueho@gmail.com>
2025-09-01 15:19:53 +0200

    clean

--- todolist/src/app/app.component.ts
+++ todolist/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './shared/components/header/header.component';
+import { HeaderComponent } from './shared/components/header/header';
 import { TodoListComponent } from './features/todos/components/todo-list.component';
 
 @Component({
